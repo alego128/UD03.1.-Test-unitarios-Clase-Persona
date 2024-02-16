7fbdb6d098c28c57fc6458cd8cf9dc854f23a94e
alego128 <152494270+alego128@users.noreply.github.com>
2024-02-16 09:53:23 +0100

    Add files via upload

---
+++ UD03.1. Test unitarios Clase Persona/src/test/java/persona/PersonaTest.java
@@ -0,0 +1,10 @@
+package persona;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonaTest {
+
+}
